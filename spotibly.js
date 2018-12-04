d42d3955b565e6ed34be0f2e77f236607ab96a6c
maxbly <max_bellamy@live.Fr>
2018-12-04 09:59:33 +0100

    upspot

--- spotibly.js
+++ spotibly.js
@@ -4,34 +4,46 @@ var qs = require("querystring");
 var fs =require("fs");
 var app = express();
 
-var burl = "https://api.spotify.com/v1"
+var burl = "https://api.spotify.com/v1/"
+
+function option (method, url, code, dataString, id) {
+	if (!id) id = "";
+	var option = {
+		url: burl+url+id,
+		method: method,
+		headers: {
+			'Accept' : 'application/json',
+			'Content-Type' : 'application/json',
+			'Authorization' : 'Bearer ' + code
+		}
+	}
+	if (dataString) option.body = JSON.stringify(dataString, null, null);
+	return option;
+}
+
+function callback (err, response, body) {
+	if (err) console.log(err);
+	console.log(body);
+}
 
 app.get('/spotibly/callback', (req, res) => {
 	res.send(req.query)
-	fs.writeFileSync("stokens.json", JSON.stringify(req.query, null, "\t"));	
+	fs.writeFileSync("stokens.json", JSON.stringify(req.query, null, "\t"));
 });
 
+
+app.get('/spotibly/devices', (req, res) => {
+	fs.readFile("stokens.json", (err, data) => {
+		var {code} = JSON.parse(data);
+		request(option('GET', 'me/player/devices', code), callback)
+	})
+})
 
 app.get('/spotibly/play', (req, res) => {
 	fs.readFile("stokens.json", (err, data) => {
 		var {id, uri} = req.query
 		var {code} = JSON.parse(data);
-		var headers = {
-			'Accept' : 'application/json',
-			'Content-Type' : 'application/json',
-			'Authorization' : 'Bearer ' + code
-		}
-		var dataString = {
-			"context_uri" : uri,
-			"position_ms" : 0
-		}
-		var option = {
-			url: burl+'/me/player/play?device_id='+id,
-			method: 'PUT',
-			headers: headers,
-			body: JSON.stringify(dataString, null, null)
-		}
-		
+		request(option('PUT', 'me/player/play?device_id=',  code, {"context_uri" : uri, "position_ms" : 0}, id), callback);
 	});
 });
 

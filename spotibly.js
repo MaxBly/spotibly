d637b079f61fd5c37a664213052f5c276d02be98
maxbly <max_bellamy@live.Fr>
2018-12-04 12:03:26 +0100

    up spo

--- spotibly.js
+++ spotibly.js
@@ -6,7 +6,7 @@ var app = express();
 
 var burl = "https://api.spotify.com/v1/"
 
-function option (method, url, code, dataString, id) {
+function option (method, url, dataString, id) {
 	if (!id) id = "";
 	var option = {
 		url: burl+url+id,
@@ -14,41 +14,55 @@ function option (method, url, code, dataString, id) {
 		headers: {
 			'Accept' : 'application/json',
 			'Content-Type' : 'application/json',
-			'Authorization' : 'Bearer ' + code
+			'Authorization' : 'Bearer ' + getCode()
 		}
 	}
 	if (dataString) option.body = JSON.stringify(dataString, null, null);
 	console.log(option);
 	return option;
 }
-
+function getCode() {
+	fs.readFile("tokens.json", (err, data) => {
+		tokens = JSON.parse(data);
+		var opt = {
+			method: 'POST',
+			url: 'https://accounts.spotify.com/api/token',
+			headers: {
+			'Authorization' : 'Basic ' + (new Buffer(tokens.id+':'+tokens.secret).toString('base64'))
+		},
+		form: {
+			grant_type: 'client_credentials'
+		},
+		json: true
+	}
+	request(opt, (err, res, body) => {
+		if(!err && res.statusCode === 200) {
+			tokens.token = body.access_token;
+			fs.writeFileSync("tokens.json", JSON.stringify(tokens))
+			return tokens.token;
+		}
+	})
+	})
+}
 function callback (err, res, body) {
 	if (err) console.log(err);
 	console.log(body);
 }
-
+/* 
 app.get('/spotibly/callback', (req, res) => {
 	res.send(req.query)
 	fs.writeFileSync("stokens.json", JSON.stringify(req.query, null, "\t"));
 	res.redirect("/")
-});
+}); */
 
 
 app.get('/spotibly/devices', (req, res) => {
-	fs.readFile("stokens.json", (err, data) => {
-		var {code} = JSON.parse(data);
-		request(option('GET', 'me/player/devices', code), callback)
-	})
-	res.redirect("/")
+	request(option('GET', 'me/player/devices'), callback)
 })
 
 app.get('/spotibly/play', (req, res) => {
-	fs.readFile("stokens.json", (err, data) => {
-		var {id, uri} = req.query
-		var {code} = JSON.parse(data);
-		request(option('PUT', 'me/player/play?device_id=',  code, {"context_uri" : uri, "position_ms" : 0}, id), callback);
-	});
-	res.redirect("/")
+	var {id, uri} = req.query
+	request(option('PUT', 'me/player/play?device_id=', {"context_uri" : uri, "position_ms" : 0}, id), callback);
 });
 
 

4108e0a0ff4c741c908aeee6bce23ae59ae6b9bd
maxbly <max_bellamy@live.Fr>
2018-12-13 17:50:22 +0100

    last maj

--- spotibly.js
+++ spotibly.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 var request = require ("request");
 var express = require ("express");
 var qs = require("querystring");
@@ -10,11 +11,11 @@ var client = require("./client.json");
 var burl = "https://api.spotify.com/v1/",
 	tokens, stateKey = 'spotify_auth_state',
 	scopes = [
-	'user-read-playback-state',
-	'user-modify-playback-state',
-	'user-read-private',
-	'user-read-email'
-];
+		'user-read-playback-state',
+		'user-modify-playback-state',
+		'user-read-private',
+		'user-read-email'
+	];
 
 function generateRandomString (length) {
 	var text = '';
@@ -47,7 +48,6 @@ app.get("/spotibly/login", (req, res) => {
 app.get("/spotibly/callback", (req, res) => {
 	var {code, state} = req.query || null;
 	var storedState = req.cookies ? req.cookies[stateKey] : null;
-
 	if (state === null || state !== storedState) {
 		res.redirect('/#' + qs.stringify({error: 'state_mismatch'}));
 	} else {
@@ -67,9 +67,9 @@ app.get("/spotibly/callback", (req, res) => {
 	    request.post(authOptions, (error, response, body) => {
 			if (!error && response.statusCode === 200) {
 				var {access_token, refresh_token, expires_in, token_type} = body;
-				tokens = {access_token, refresh_token, expires_in, token_type}
+				tokens = {access_token, refresh_token, expires_in, token_type};
 				fs.writeFileSync("tokens.json", JSON.stringify(tokens, null, "\t"));
-				res.send(tokens)
+				res.send(JSON.stringify(tokens, null, "\t"));
 			} else {
 				res.redirect('/#' +
 				qs.stringify({
@@ -81,28 +81,30 @@ app.get("/spotibly/callback", (req, res) => {
 });
 
 app.get("/spotibly/refresh_token", (req, res) => {
-
-	// requesting access token from refresh token
-	var {refresh_token} = req.query;
-	var authOptions = {
-		url: 'https://accounts.spotify.com/api/token',
-		headers: { 'Authorization': 'Basic ' + (new Buffer(client.id + ':' + client.secret).toString('base64')) },
-		form: {
-	    	grant_type: 'refresh_token',
-	    	refresh_token: refresh_token
-		},
-		json: true
-	};
-
-	request.post(authOptions, (error, response, body) => {
-    	if (!error && response.statusCode === 200) {
-			var {access_token, token_type, expires_in} = body;
-			res.send(tokens);
-			tokens = {access_token, refresh_token, token_type, expires_in};
-			fs.writeFileSync('./tokens.json', JSON.stringify(tokens, null, "\t"))
-		}
+	fs.readFile("./tokens.json", (err, data) => {
+		tokens = JSON.parse(data)
+		var authOptions = {
+			url: 'https://accounts.spotify.com/api/token',
+			headers: { 'Authorization': 'Basic ' + (new Buffer(client.id + ':' + client.secret).toString('base64')) },
+			form: {
+				grant_type: 'refresh_token',
+				refresh_token: tokens.refresh_token
+			},
+			json: true
+		};
+		console.log(authOptions)
+		request.post(authOptions, (error, response, body) => {
+    		if (!error && response.statusCode === 200) {
+				var {access_token, token_type, expires_in} = body;
+				var refresh_token = tokens.refresh_token;
+				res.send(body);
+				console.log(JSON.stringify(tokens, null, "\t"));
+				tokens = {access_token, refresh_token, token_type, expires_in};
+				fs.writeFileSync('./tokens.json', JSON.stringify(tokens, null, "\t"))
+			}
+		});
 	});
-})
+});
 
 function option (method, url, code, dataString, id) {
 	if (!id) id = "";
@@ -120,22 +122,18 @@ function option (method, url, code, dataString, id) {
 	return option;
 }
 
-function callback (err, res, body) {
-	if (err) console.log(err);
-	console.log(body);
-}
-
-
-
 app.get('/spotibly/devices', (req, res) => {
 	fs.readFile("./tokens.json", (err, data) => {
 		tokens = JSON.parse(data);
 		request(option('GET', 'me/player/devices', tokens.access_token), (err, response, body) => {
-			console.log(body)
-			res.send(JSON.stringify(body, null, "\t"))
+			console.log(response.statusCode)
+			if (response.statusCode == 200) {
+				//console.log(body)
+				res.send(JSON.stringify(body, null, "\t"))
+			}
 		});
-	})
-})
+	});
+});
 
 app.get('/spotibly/play', (req, res) => {
 	var {id, uri} = req.query
@@ -143,25 +141,27 @@ app.get('/spotibly/play', (req, res) => {
 		tokens = JSON.parse(data)
 		request(
 			option('PUT', 'me/player/play?device_id=', tokens.access_token, {"context_uri" : uri, "position_ms" : 0}, id),
-			(err, response, body) =>{
+			(err, response, body) => {
+				console.log(response.statusCode)
 				console.log(body);
 				res.send(JSON.stringify(body, null, "\t"));
 			}
 		);
-	})
+	});
 });
 
-app.get('/spotibly/next', (res, req) => {
+app.get('/spotibly/next', (req, res) => {
 	var {id} = req.query
 	fs.readFile("./tokens.json", (err, data) => {
-		tokens = JSON.parse(tokens)
+		tokens = JSON.parse(data)
 		request(
-			option('POST', 'me/player/next', tokens.access_token, null, id),
+			option('POST', 'me/player/next?device_id=', tokens.access_token, null, id),
 			(err, response, body) => {
+				console.log(response.statusCode)
 				console.log(body);
 				res.send(JSON.stringify(body, null, "\t"));
 			}
-		)
+		);
 	})
 })
 
